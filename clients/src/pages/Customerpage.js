f8f5acfc586a4f2cb191275fc3f66a80f6ec2242
Abdinasir <samiradynassir@gmail.com>
2024-05-28 07:13:38 +0300

    added customer page

--- clients/src/pages/Customerpage.js
+++ clients/src/pages/Customerpage.js
@@ -1,13 +1,43 @@
 // 
 import React, {useEffect, useState} from 'react'
 import DefaultLayout from '../components/DefaultLayout'
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { Table } from 'antd';
+
 
 const Customerpage = () => {
     const [billsData, setBillsData] = useState([]);
+    const dispatch = useDispatch();
+    const getAllBills = async () => {
+        try {
+            dispatch({ type: "SHOW_LOADING" });
+            const { data } = await axios.get('/api/bills/get-bills');
+            setBillsData(data);
+            dispatch({ type: "HIDE_LOADING" });
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getAllBills();
+    }, []);
+    // table columns
+    const columns = [
+        { title: 'ID', dataIndex: '_id' },
+        { title: 'customerName', dataIndex: 'customerName' },
+        { title: 'CustomerNumber', dataIndex: 'CustomerNumber' },
+        
+        
+    ];
 
   return (
     <DefaultLayout>
         <h1>Customers Page</h1>
+        <Table columns={columns} dataSource={billsData} bordered pagination={false} />
+
     </DefaultLayout>
   )
 }
